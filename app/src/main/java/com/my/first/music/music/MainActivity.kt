e3ab03fea70ef90158403addced776ea72b7517f
Jyoti <iamjyotisharma12@gmail.com>
2023-09-13 16:11:41 +0530

    jyoti

---
+++ app/src/main/java/com/my/first/music/music/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.my.first.music.music
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.my.first.music.music.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Music)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.shuffleBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, PlayerActivity::class.java)
+            startActivity(intent)
+        }
+        binding.favouriteBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, FavouriteActivity::class.java)
+            startActivity(intent)
+        }
+        binding.PlaylistBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, playlistActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/my/first/music/music/playlistActivity.kt
@@ -0,0 +1,11 @@
+package com.my.first.music.music
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class playlistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlist)
+    }
+}
